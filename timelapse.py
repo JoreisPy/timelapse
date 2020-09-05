8afe2353b474d0b7d71a4e06468e2f5375a1b60b
Joreis <joao.reis@protonmail.ch>
2020-09-05 12:46:39 +0200

    added option to compile the video or not.

--- timelapse.py
+++ timelapse.py
@@ -2,11 +2,12 @@ from picamera import PiCamera
 from os import system
 import datetime
 from time import sleep
+import sys
 
 tlminutes = 10 #set this to the number of minutes you wish to run your timelapse camera
 secondsinterval = 1 #number of seconds delay between each photo taken
 fps = 30 #frames per second timelapse video
-numphotos = int((tlminutes*60)/secondsinterval) #number of photos to take
+numphotos = 10 #int((tlminutes*60)/secondsinterval) #number of photos to take
 print("number of photos to take = ", numphotos)
 
 dateraw= datetime.datetime.now()
@@ -14,7 +15,7 @@ datetimeformat = dateraw.strftime("%Y-%m-%d_%H:%M")
 print("RPi started taking photos for your timelapse at: " + datetimeformat)
 
 camera = PiCamera()
-camera.resolution = (1024, 768)
+camera.resolution = (1920, 1080)
 
 system('rm /home/pi/Pictures/*.jpg') #delete all photos in the Pictures folder before timelapse start
 
@@ -22,8 +23,13 @@ for i in range(numphotos):
     camera.capture('/home/pi/Pictures/image{0:06d}.jpg'.format(i))
     sleep(secondsinterval)
 print("Done taking photos.")
-print("Please standby as your timelapse video is created.")
 
-system('ffmpeg -r {} -f image2 -s 1024x768 -nostats -loglevel 0 -pattern_type glob -i "/home/pi/Pictures/*.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p /home/pi/Videos/{}.mp4'.format(fps, datetimeformat))
-#system('rm /home/pi/Pictures/*.jpg')
-print('Timelapse video is complete. Video saved as /home/pi/Videos/{}.mp4'.format(datetimeformat))
+make_video = (input("would you like to compile the video? (y/n) -- If you are using a piZero (no) is recommended "))
+
+if make_video == "y" or "yes":
+    print("Please standby as your timelapse video is created.")
+    system('ffmpeg -r {} -f image2 -s 1024x768 -nostats -loglevel 0 -pattern_type glob -i "/home/pi/Pictures/*.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p /home/pi/Videos/{}.mp4'.format(fps, datetimeformat))
+    print('Timelapse video is complete. Video saved as /home/pi/Videos/{}.mp4'.format(datetimeformat))
+
+else:
+    sys.exit("Video not compiled")
